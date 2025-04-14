cb14e23c6abf4faffc56a48058afb7ff5aa842fc
Rozhkov 23 <stass211183@gmail.com>
2025-04-14 20:08:29 +0400

    Задание №6

---
+++ src/Task6.java
@@ -0,0 +1,12 @@
+public class Task6 {
+    public static void main(String[] args) {
+        System.out.println("Задание №6 ");
+        final int W = 3;
+        final int H = 5;
+        int s = W * H;
+        int p = (W + H) * 2;
+        System.out.println("Square = " + s);
+        System.out.println("Perimeter = " + p);
+
+    }
+}

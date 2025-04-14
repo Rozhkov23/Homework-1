2c994ab1ed90b5ab1c877cda2a1889c6e088504b
Rozhkov 23 <stass211183@gmail.com>
2025-04-14 21:20:52 +0400

    Задание №9

---
+++ src/Task9.java
@@ -0,0 +1,10 @@
+public class Task9 {
+    public static void main(String[] args) {
+        System.out.println("Задание №9");
+        double number = 8.0;
+        String answer = ((number - (int) number) == 0) ? "целое число" : "a fractional number!";
+        System.out.println(answer);
+
+        }
+    }
+
